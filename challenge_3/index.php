7c6abd9f75263de256068a9032e6ab2677798458
Ian Powell <ianpowell77@yahoo.com>
2017-09-26 11:29:45 -0400

    test

--- challenge_3/index.php
+++ challenge_3/index.php
@@ -5,9 +5,13 @@ require("inc/db_inc.php");
 		try{
 			$sql = "INSERT INTO products (product_name, description, price, color)";
 			$sql .= "VALUES(:name, :description, :price, :color)";
+			$_POST["name"] = filter_input(INPUT_POST, $_POST["name"],FILTER_SANITIZE_STRING);
+			$_POST["description"] = filter_input(INPUT_POST, $_POST["description"],FILTER_SANITIZE_STRING);
+			$_POST["price"] = filter_input(INPUT_POST, $_POST["price"],FILTER_SANITIZE_NUMBER_FLOAT);
+			$_POST["color"] = filter_input(INPUT_POST, $_POST["color"],FILTER_SANITIZE_STRING);
 			$stmt = $db->prepare($sql);
 			$stmt->execute(array(
-		      ':name' => filter_input(INPUT_POST, $_POST["name"],FILTER_SANITIZE_STRING),
+		      ':name' => $_POST["name"],
 		      ':description' => $_POST["description"],
 		      ':price' => $_POST["price"],
 		      ':color' => strtolower($_POST["color"]),
