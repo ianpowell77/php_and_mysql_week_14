0e0c2466fdd1814bee60cbb6fa9d96ab805d0d7e
Ian Powell <ianpowell77@yahoo.com>
2017-09-26 11:43:56 -0400

    reverted changes

--- challenge_3/index.php
+++ challenge_3/index.php
@@ -6,15 +6,12 @@ require("inc/db_inc.php");
 			$sql = "INSERT INTO products (product_name, description, price, color)";
 			$sql .= "VALUES(:name, :description, :price, :color)";
 			$stmt = $db->prepare($sql);
-			$name = filter_input(INPUT_POST, $_POST["name"],FILTER_SANITIZE_STRING);
-			$description = filter_input(INPUT_POST, $_POST["description"],FILTER_SANITIZE_STRING);
-			$price = filter_input(INPUT_POST, $_POST["price"],FILTER_SANITIZE_NUMBER_FLOAT);
-			$color = filter_input(INPUT_POST, $_POST["color"],FILTER_SANITIZE_STRING);
-			$stmt->bindParam(':name', $name, PDO::PARAM_STR);
-			$stmt->bindParam(':description', $description, PDO::PARAM_STR);
-			$stmt->bindParam(':price', $price, PDO::PARAM_STR);
-			$stmt->bindParam(':color', $color, PDO::PARAM_STR);
-			$stmt->execute();
+			$stmt->execute(array(
+		      ':name' => $_POST["name"],
+		      ':description' => $_POST["description"],
+		      ':price' => $_POST["price"],
+		      ':color' => strtolower($_POST["color"]),
+		    ));
 		} catch (Exception $e) {
 			echo $e->getMessage();
 		}
