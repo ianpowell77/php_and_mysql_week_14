923630ae1331c4ff9b2d65b8daa44a3999b94ad9
Ian Powell <ianpowell77@yahoo.com>
2017-09-26 09:35:46 -0400

    updated database connection for ninja server

--- challenge_3/inc/db_inc.php
+++ challenge_3/inc/db_inc.php
@@ -1,7 +1,7 @@
 <?php
 try{ // make db connection
         // This is the new standard db connection
-        $db = new PDO('mysql:host=localhost;dbname=challenge_2','root','root');
+        $db = new PDO('mysql:host=localhost;dbname=ipowell_week14','r2hstudent','SbFaGzNgGIE8kfP');
         $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         //Turn OFF emulated prepared statements!
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
